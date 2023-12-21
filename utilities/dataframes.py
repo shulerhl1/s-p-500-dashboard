cac43d86e8506b8ef7f999374f26a3a1ffccf6a2
shulerhl1 <154532578+shulerhl1@users.noreply.github.com>
2023-12-21 10:47:58 -0500

    Update dataframes.py

--- utilities/dataframes.py
+++ utilities/dataframes.py
@@ -69,8 +69,8 @@ def table_returns(value):
 
 
 df = pd.read_excel(
-    io=get_recent_file(),
-    # io=r'http://www.econ.yale.edu/~shiller/data/ie_data.xls'
+    # io=get_recent_file(),
+    io=r'http://www.econ.yale.edu/~shiller/data/ie_data.xls'
     sheet_name='Data',
     header=None,
     usecols='A:D',
